032a2cb5a500d082dad01ce21fe6e48fa6b8a2c1
Joseph <nguld@itsj-group.com>
2021-01-20 16:11:55 +0700

    added monitor service

---
+++ monitor/src/main/java/com/example/monitor/port/persistence/LogRepository.java
@@ -0,0 +1,26 @@
+package com.example.monitor.port.persistence;
+
+import com.example.monitor.domain.PastEvent;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LogRepository {
+
+  public static LogRepository instance = new LogRepository();
+
+  private Map<String, List<PastEvent>> events = new HashMap<>();
+
+  public Map<String, List<PastEvent>> getAllPastEvents() {
+    return events;
+  }
+
+  public void addEvent(PastEvent pastEvent) {
+    if (!events.containsKey(pastEvent.getTransactionId())) {
+      events.put(pastEvent.getTransactionId(), new ArrayList<>());
+    }
+    events.get(pastEvent.getTransactionId()).add(pastEvent);
+  }
+}
---
+++ monitor/src/main/java/com/example/monitor/web/MonitorRestController.java
@@ -0,0 +1,19 @@
+package com.example.monitor.web;
+
+import com.example.monitor.domain.PastEvent;
+import com.example.monitor.port.persistence.LogRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class MonitorRestController {
+
+  @RequestMapping(path = "/event", method = RequestMethod.GET)
+  public Map<String, List<PastEvent>> getAllEvents() {
+    return LogRepository.instance.getAllPastEvents();
+  }
+}
